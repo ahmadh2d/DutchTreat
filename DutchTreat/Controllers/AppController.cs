8085b8920108c79e64e31fba9fbcdf98717151b4
ahmadh2d <38376104+ahmadh2d@users.noreply.github.com>
2020-08-19 23:30:11 +0500

    Enabled MVC and  Dev Exception page

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -2,12 +2,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 
 namespace DutchTreat.Controllers
 {
 	public class AppController : Controller
 	{
-
+		public IActionResult Index()
+		{
+			//throw new InvalidOperationException();
+			return View();
+		}
 	}
 }
